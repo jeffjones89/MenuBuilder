d8b3e48b80a7080acb002ef679179d2978e7b4e6
Jeff Jones <jeff.jones1@gmail.com>
2015-08-25 09:24:23 -0400

    edited names for Item class

--- public/javascripts/models/item.js
+++ public/javascripts/models/item.js
@@ -1,19 +1,18 @@
 var Item = function(info){
   var self = this;
   this.item_name = info.item_name;
-  this.nf_calories = info.nf_calories;
-  this.nf_protein = info.nf_protein;
-  this.nf_total_fat = info.nf_total_fat;
-  this.nf_total_carbohydrate = info.nf_total_carbohydrate;
+  this.calories = info.nf_calories;
+  this.protein = info.nf_protein;
+  this.total_fat = info.nf_total_fat;
+  this.carbohydrates = info.nf_total_carbohydrate;
 };
 Item.fetch = function(){
   var submit = $('input[type=submit]');
   submit.on('click', function(){
   event.preventDefault();
-  var restaurant = $('#menu-search');
-  var url = "https://api.nutritionix.com/v1_1/search/"+ restaurant.val() +"?results=0%3A20&cal_min=450&cal_max=50000&fields=*&appId=32443fbb&appKey=37587913d9144f31f691efdf130a12ab"
-  var request = $.getJSON(url).
-  then(function(response){
+  var restaurant = $('#menu-search').val();
+  var url = "https://api.nutritionix.com/v1_1/search/"+ restaurant +"?results=0%3A20&cal_min=450&cal_max=50000&fields=*&appId=32443fbb&appKey=37587913d9144f31f691efdf130a12ab"
+  var request = $.getJSON(url).then(function(response){
     $('#menu-search').val('');
     var itemsArr = response.hits;
     var items = [];
@@ -24,5 +23,6 @@ Item.fetch = function(){
   }).then(function(items){
     console.log(items);
   });
+  return request;
 });
 };
