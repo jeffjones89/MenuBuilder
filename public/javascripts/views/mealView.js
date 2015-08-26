b23b0a50d537d69cdc4edbd1e0ff2fd7214eb3ac
Jeff Jones <jeff.jones1@gmail.com>
2015-08-26 17:03:34 -0400

    pre merge commit

--- public/javascripts/views/mealView.js
+++ public/javascripts/views/mealView.js
@@ -2,9 +2,13 @@ var mealView = {
   $el: $("#meal"),
   meal: [],
   appendItem: function(item){
-  $("#meal").append('<div class = mealItem id = ' + item.id + ">")
-  var html = $('#'+ item.id);
-  html.append('<h5>' + item.itemName + '</h5>');
-  
+    $("#meal").append('<div class = mealItem id = ' + item.id + ">");
+    var html = $('#'+ item.id);
+      html.append('<h5>' + item.itemName + '</h5>');
+      html.append("<h5>" + 'Brand Name: ' + item.brandName + "</h5>");
+      html.append("<h5>" + 'Calories: '+ item.calories + "</h5>");
+      html.append("<h5>" + 'Protein: ' + item.protein + "</h5>");
+      html.append("<h5>"+ 'Total Fat: ' + item.totalFat + "</h5>");
+      html.append("<h5>" + 'Carbs: '+  item.carbohydrates + "</h5>");
   }
 }
