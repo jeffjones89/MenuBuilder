0ab627f487d73c4d3a50fcbb17413c14ae796c25
Jeff Jones <jeff.jones1@gmail.com>
2015-08-26 10:53:36 -0400

    passing items to meal array

--- public/javascripts/views/itemView.js
+++ public/javascripts/views/itemView.js
@@ -8,20 +8,27 @@ ItemView.prototype = {
     var self = this;
     self.$el.html(self.itemTemplate(self.item));
     $(".items").append(self.$el);
+
+    var add = self.$el.find('.addItem');
+    add.on('click', function(){
+      var itemObj = self.item;
+      self.addToMeal(itemObj);
+    });
   },
   itemTemplate: function(item){
     var html = $("<div>");
-    html.append("<h6>" +  item.item_name + "</h6>")
+    html.append("<h6>" +  item.item_name + "</h6>");
     html.append("<h5>" + 'Calories: '+ item.calories + "</h5>");
     html.append("<h5>" + 'Protein: ' + item.protein + "</h5>");
     html.append("<h5>"+ 'Total Fat: ' + item.total_fat + "</h5>");
     html.append("<h5>" + 'Carbs: '+  item.carbohydrates + "</h5>");
-    html.append("<button id = 'add_to_meal' type = 'button'>Add to Meal</button>");
-    return(html)
+    html.append("<button class = 'addItem' type = 'button'>Add to Meal</button>");
+    return(html);
   },
-  addToMeal: function(){
-    // var self = this;
-    //
-    // if()
+  addToMeal: function(item){
+    var meal = [];
+    meal.push(item);
+    console.log(meal);
+    return meal;
   }
 }
