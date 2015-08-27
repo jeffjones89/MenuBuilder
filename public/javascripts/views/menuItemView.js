884cfcfa46efa954626904c4c2f9e4f32e3d0c78
Jeff Jones <jeff.jones1@gmail.com>
2015-08-27 10:37:12 -0400

    adding show/hide functionality to project

--- public/javascripts/views/menuItemView.js
+++ public/javascripts/views/menuItemView.js
@@ -11,6 +11,7 @@ MenuView.prototype = {
     var add = self.$el.find('.addItem');
     var show = self.$el.find('.itemName');
 
+    self.hideDivs();
 
     add.on('click', function(){
       var itemObj = self.item;
@@ -18,21 +19,35 @@ MenuView.prototype = {
       meal.appendItem(itemObj);
       meal.render();
     });
+
+    show.on('click', function(){
+      self.showDiv();
+    })
   },
   itemTemplate: function(item){
+    var self = this;
     var html = $("<div>");
-    html.append("<h4>" +  item.itemName + "</h4>");
-    html.append("<h5 class='data'>" + 'Brand Name: ' + item.brandName + "</h5>");
-    html.append("<h5 class='data'>" + 'Calories: '+ item.calories + "</h5>");
-    html.append("<h5 class='data'>" + 'Protein: ' + item.protein + "</h5>");
-    html.append("<h5 class='data'>"+ 'Total Fat: ' + item.totalFat + "</h5>");
-    html.append("<h5 class='data'>" + 'Carbs: '+  item.carbohydrates + "</h5>");
-    html.append("<button class = 'addItem' type = 'button'>Add to Meal</button>");
+      html.append("<h4 class = itemName>" +  item.itemName + "</h4>");
+      html.append("<div class = itemInfo>");
+    var itemInfo = html.find(".itemInfo");
+      itemInfo.append("<h5 class='data'>" + 'Brand Name: ' + item.brandName + "</h5>");
+      itemInfo.append("<h5 class='data'>" + 'Calories: '+ item.calories + "</h5>");
+      itemInfo.append("<h5 class='data'>" + 'Protein: ' + item.protein + "</h5>");
+      itemInfo.append("<h5 class='data'>"+ 'Total Fat: ' + item.totalFat + "</h5>");
+      itemInfo.append("<h5 class='data'>" + 'Carbs: '+  item.carbohydrates + "</h5>");
+      itemInfo.append("<button class = 'addItem' type = 'button'>Add to Meal</button>");
     return(html);
   },
   addToMeal: function(item){
     mealArr = meal.items;
     mealArr.push(item);
     return mealArr;
+  },
+  showDiv: function(){
+    var self = this;
+    self.$el.find('.itemInfo').fadeIn(450);
+  },
+  hideDivs: function(){
+    $('.itemInfo').hide();
   }
 }
