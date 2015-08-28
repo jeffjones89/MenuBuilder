0548a46d3f9107e0083a467839000fe87f885e18
Cameron Fry <cameronfry11@gmail.com>
2015-08-28 10:34:35 -0400

    Began twitter authorization

---
+++ env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  consumerKey: "g8eBak5Gs5NVOXPj17qvQGKbw",
+  consumerSecret: "uzKTYOZ6HDuhHFjLzutKdPfgXFNe7Z4Lf7KUBG91LZNLTtXQGB",
+  callbackUrl: "http://127.0.0.1:3001/auth/twitter/callback" // be sure to use http://127.0.0.1 not localhost
+};
--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var bodyParser = require("body-parser");
+var env = require("./env")
+var session = require("express-session")
 
+app.use(express.static("public"))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(path.join(__dirname + "/public")));
@@ -14,6 +17,40 @@ app.get("/", function(req, res){
 });
 
 app.use("/", mealsController);
+
+// TWITTER AUTH
+
+var passport = require("passport")
+var TwitterStrategy = require("passport-twitter").Strategy
+passport.serializeUser(function(user, done) {
+  done(null, user)
+})
+passport.deserializeUser(function(obj, done) {
+  done(null, obj)
+})
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new TwitterStrategy({
+  consumerKey: env.consumerKey,
+  consumerSecret: env.consumerSecret,
+  callbackUrl: env.callbackUrl
+}, function(aToken, aTokenSecret, aProfile, done){
+  token = aToken
+  tokenSecret = aTokenSecret
+  profile = aProfile
+  done(null, profile)
+}))
+
+app.get("/auth/twitter", passport.authenticate("twitter"), function(req, res){
+})
+
+app.get("/auth/twitter/callback", passport.authenticate('twitter'), function(req, res){
+ req.session.token = token
+ req.session.tokenSecret = tokenSecret
+ req.session.profile = profile
+ res.redirect("/posts")
+})
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port 3000");
