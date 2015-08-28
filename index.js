b01d17dfb9b4ec0b1fb127e74c7eafae36dae790
Cameron Fry <cameronfry11@gmail.com>
2015-08-28 11:30:31 -0400

    completed very basic twitter auth

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(path.join(__dirname + "/public")));
 app.set("view engine", "hbs");
+app.use(session({secret: "pizza jam"}))
 
 var mealsController = require("./config/controllers/meals")
 app.get("/", function(req, res){
@@ -42,14 +43,13 @@ passport.use(new TwitterStrategy({
   done(null, profile)
 }))
 
-app.get("/auth/twitter", passport.authenticate("twitter"), function(req, res){
-})
+app.get("/auth/twitter", passport.authenticate("twitter"))
 
 app.get("/auth/twitter/callback", passport.authenticate('twitter'), function(req, res){
  req.session.token = token
  req.session.tokenSecret = tokenSecret
  req.session.profile = profile
- res.redirect("/posts")
+ res.redirect("/")
 })
 
 app.listen(process.env.PORT || 3000, function(){
